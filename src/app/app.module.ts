2a8df8d690dec5ddac0c56e4f443ad454c0bf143
Pedro Nordenflycht <pedronordenflycht@MacBook-Air-de-Pedro.local>
2025-05-04 19:35:34 -0400

    Carpeta components creada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
