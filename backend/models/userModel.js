aba577686cb8d9460ad8ad6ead5d8ded898f169d
nachoavila <ignacio.avila215@gmail.com>
2025-06-04 23:32:16 -0400

    user model hecho

---
+++ backend/models/userModel.js
@@ -0,0 +1,16 @@
+const { use } = require('react');
+const db = require('../config/db');
+
+const UserModel = {
+    getAll: (callback) => {
+        db.query('SELECT * FROM usuarios', callback);
+    },
+    create: (user, callback) => {
+        db.query('INSERT INTO usuarios SET ?', user, callback);
+    },
+    findByUsername: (username, callback) => {
+        db.query('SELECT * FROM usuarios WHERE username = ?', [username], callback);
+    }
+};
+
+module.exports = UserModel;
